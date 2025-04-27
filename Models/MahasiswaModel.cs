f735f755ce6ebbfdd06318b91e7ed8cfe406fd05
Tdarrelpa <darrel2208@gmail.com>
2025-04-27 13:28:59 +0700

    Menambahkan API mahasiswa

---
+++ Models/MahasiswaModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tpmodul10_103022300144.Models
+{
+    public class MahasiswaModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Nim { get; set; }
+    }
+}
